d2977ab13c9573c73933cbb1de40a1a7c4094513
akhilesh <akhileshseechurn@gmail.com>
2020-06-03 00:46:14 +0400

    [Problem 5] Smallest multiple refactoring

--- Problem 5/solution.py
+++ Problem 5/solution.py
@@ -1,6 +1,8 @@
+import time
+
 def solution(n):
     lst = []
-    for i in range(2, n+1):
+    for i in range(n, 2, -1):
         lcm = calculate_lcm(i)
         lst.append(lcm)
     result = 1
@@ -13,16 +15,41 @@ def solution(n):
     return result
 
 def calculate_lcm(n):
+    lcm = fetch_from_cache(n)
+    if (lcm != None):
+        return lcm
     i = 2
     lcm = []
+    keys = []
     while i * i <= n:
+        temp = fetch_from_cache(n)
+        if (temp != None):
+            update_cache(keys, list(lcm))
+            return lcm + temp
         if n % i:
             i += 1
         else:
             n /= i
+            keys.append(int(n))
             lcm.append(int(i))
     if n > 1:
+        keys.append(int(n))
         lcm.append(int(n))
+    update_cache(keys, list(lcm))
     return lcm
 
+def update_cache(n, lcm):
+    if (not caching_enable):
+        return
+    for i in range(len(n)-1):
+        lcm_cache[n[i]] = list(lcm)
+        lcm.pop(0)
+
+def fetch_from_cache(n):
+    if (not caching_enable):
+        return None
+    return lcm_cache.get(n)
+
+lcm_cache = {}
+caching_enable = True
 print(solution(20))
