6cbe625cbbbb7cdf41300017ab2674c68c4c4be2
akhilesh <akhileshseechurn@gmail.com>
2020-06-10 00:32:38 +0400

    [Problem 9] Optimize special Pythagorean triplet

--- Problem 9/solution.py
+++ Problem 9/solution.py
@@ -1,11 +1,9 @@
 def solution(n):
     for a in range(1, n):
         for b in range(1, n):
-            for c in range(1, n):
-                if (a + b + c == n and a**2 + b**2 == c**2):
-                    print("a", a)
-                    print("b", b)
-                    print("c", c)
-                    return a*b*c
-                    
+            d = a**2 + b**2
+            c = d**(1/2)
+            if (a + b == n - c):
+                return int(a*b*c)
+
 print(solution(1000))
